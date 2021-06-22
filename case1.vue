d95f21e002d5bdc24a9257989717f4209d4138ed
Владимир <ulladimil@gmail.com>
2021-06-22 11:56:08 +0300

    Create case1.vue

---
+++ case1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="sql-editor-component">
+        <div
+            ref="editor"
+            class="editor language-sql h-100"
+        />
+    </div>
+</template>
+
+<script>
+import { CodeJar } from 'codejar';
+import Prism from 'prismjs';
+
+export default {
+    name: 'SQLEditorComponent',
+
+    data() {
+        return {
+            editor: null
+            template: null
+        };
+    },
+
+    mounted() {
+        this.editor = CodeJar(this.$refs.editor, Prism.highlightElement);
+        this.editor.onUpdate(text => this.template.sql = text);
+    }
+} 
+</script>
