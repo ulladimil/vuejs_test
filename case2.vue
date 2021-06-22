f1bcfb681212ee2d8ce15ebe06c11f7f842215aa
Владимир <ulladimil@gmail.com>
2021-06-22 12:13:51 +0300

    Create case2.vue

---
+++ case2.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="some-counter">
+        Count: {{ count }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SomeCounter',
+
+    data() {
+        return {
+            timer: null,
+            count: 1
+        };
+    },
+
+    created: () => {
+        this.timer = setInterval(() => this.count++, 1000);
+    },
+
+    beforeDestroy: () => {
+        clearInterval(this.timer);
+    }
+} 
+</script>
